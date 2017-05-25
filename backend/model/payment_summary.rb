2a02e40632f3da5806d7f443cad4b9ae5b6abe48
Mark Cooper <markchristophercooper@gmail.com>
2017-05-25 15:31:52 -0700

    Bring in defae760a77684bd5a7e91e0ed8bb7956bdb16d4

--- backend/model/payment_summary.rb
+++ backend/model/payment_summary.rb
@@ -7,4 +7,8 @@ class PaymentSummary < Sequel::Model(:payment_summary)
   set_model_scope :global
 
   include Payments
+
+  def self.update_toplevel_mtimes(*)
+    # Just here as a bugfix for versions 2.0.0 -- 2.0.1.
+  end
 end
